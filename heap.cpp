13eb9a90207d30842b5517dfd3fbaee78b23db34
George <piatskgeorge@mail.ru>
2019-05-20 01:06:45 +0300

    Create heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,9 @@
+template <typename T, typename TPred>
+void heapify(T& a, size_t size, size_t i, TPred pred)
+{
+}
+
+template <typename T, typename TPred>
+void buildHeap(T& a, size_t size, TPred pred)
+{
+}
