29e237f604e855ba038cad3547292889811f0bb0
George <piatskgeorge@mail.ru>
2019-05-20 01:15:37 +0300

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -5,7 +5,7 @@ void heapsort(T& a, size_t size, TPred pred)
 
     for (size_t i = size - 1; i >= 1; i--)
     {
-        swap(a, i, 0);
+        std::swap(a[i], a[0]);
         size--;
         heapify(a, size, 0, pred);
     }
